83df7e244786f09e837e1adda9237e05f0f46ea2
Esteban Vargas <estebanvargas10@gmail.com>
2018-06-17 00:14:53 -0600

    Mongo Added

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
     new GoogleStrategy({
@@ -9,10 +12,19 @@ passport.use(
             callbackURL: "/auth/google/callback"
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log("===Access Token===");
-            console.log("Access token", accessToken);
-            console.log("refreshToken", refreshToken);
-            console.log("profile", profile);
+            User.findOne({
+                    googleId: profile.id
+                })
+                .then(((existingUser) => {
+                    if (existingUser) {
+                        console.log("The user exists")
+
+                    } else {
+                        new User({
+                            googleId: profile.id
+                        }).save();
+                    }
+                }))
         }
     )
 );
