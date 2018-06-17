46edc214fe996beea4c0fa200f8e7c896c0690fb
EstebanVargas <estebanvargas10@gmail.com>
2018-06-16 18:32:22 -0600

    From git

--- server/index.js
+++ server/index.js
@@ -2,11 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+  res.send({ hi: 'there' });
+});
 
-})
-
-//El de variable o el puerto por defecto
-// const PORT = process.env.PORT || 5000;
-var port = process.env.PORT || 5000
-app.listen(port);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
